af51da5ab45d7d631c793489c8b68eb537099f86
BahadirKayis <bahadir.kayis24@gmail.com>
2023-03-02 13:13:13 +0300

    Initial commit

---
+++ app/src/main/java/com/bahadir/wordle/common/Constants.kt
@@ -0,0 +1,12 @@
+package com.bahadir.wordle.common
+
+object Constants {
+    const val BASE_URL_WORD = "https://api.dictionaryapi.dev/api/v2/"
+
+    const val BASE_URL_SYNONYMS = "https://api.datamuse.com/"
+
+    const val WORDS = "entries/en/"
+    
+    const val SYNONYMS = "words"
+
+}
